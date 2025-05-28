2a71fcf33e1ef6ee0ea6cc4c254ffaef5bb96ef8
Mouhcine Attif <mouhcineattif@gmail.com>
2025-05-28 06:17:08 +0100

    add qr_code and role fields to users table

--- locals_location_system/database/migrations/0001_01_01_000000_create_users_table.php
+++ locals_location_system/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('qr_code');
+            $table->enum('role', ['admin', 'client'])->default('client');
             $table->rememberToken();
             $table->timestamps();
         });
